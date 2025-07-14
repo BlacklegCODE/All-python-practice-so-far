30d565666f3770d2df974b947d4735883f0e71de
Harsh Raundal <dkingston0000@gmail.com>
2025-07-14 21:25:09 +0530

    Create more basic operations on strings.py

---
+++ more basic operations on strings.py
@@ -0,0 +1,39 @@
+a1 = "I am Harsh, !"
+a2 = " Harsh"
+print(a2 in a1)
+
+print(a1 + a2)
+
+aa1 = " I am pain"
+aa2 = "pain"
+
+print(aa1.index(aa2))
+
+print(len(aa1))
+
+r1= aa1.upper()
+r2 = aa1.lower()
+
+print(r1)
+print(r2)
+
+print(r1.isupper())
+print(r2.islower())
+
+print(aa1.startswith("I"))
+print(aa1.startswith(" "))
+
+print(aa1.split())
+
+l = ["I","AM","NOT","PAIN","Better"]
+print(" ".join(l))
+print(",".join(l))
+
+bro = "----Harsh----"
+print(bro.strip("-"))
+
+print(bro.lstrip("-"))
+print(bro.rstrip("-"))
+
+print(a1.find("Harsh"))
+print(aa1.find("pain"))
