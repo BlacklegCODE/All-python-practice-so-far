ae6f3ad33b758d9ff6f748dd3f1c9d7ed8ad438b
Harsh Raundal <dkingston0000@gmail.com>
2025-07-14 20:44:05 +0530

    Create formatted string.py

---
+++ formatted string.py
@@ -0,0 +1,10 @@
+h1 = 5
+h2 = 6
+print(f"Product of {h1} and {h2} will be {h1 * h2})
+print(f"Addition of {h1} and {h2} will be {h1 + h2})
+h = """
+This is the story of my life
+....
+Thats it !
+"""
+print(f"{h} Thats something new !")
